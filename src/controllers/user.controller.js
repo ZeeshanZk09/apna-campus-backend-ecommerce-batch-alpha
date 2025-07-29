eb5451de1d85966d37a630104008c2b0959c06e5
ZeeshanZk09 <mzeeshankhan0988@gmail.com>
2025-07-29 21:22:31 +0500

    bcrypt ko bcryptjs se replace kiya, user registration aur login ke liye password hashing aur validation ka logic update kiya. package.json aur package-lock.json mein dependencies ko update kiya.

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -2,39 +2,38 @@ import { User } from '../models/user.model.js';
 import { ApiError } from '../utilities/ApiError.js';
 import requestHandler from '../utilities/requestHandler.js';
 import { ApiResponse } from '../utilities/ApiResponse.js';
-// create
-// update
-// updateById
-//
+import bcrypt from 'bcryptjs';
+
 const userRegister = requestHandler(async (req, res, next, err) => {
-  // register krny ke lia kia kia chahai or jo chahia wo aay ga kahan se
-  //  username, email, password
   const { username, email, password } = req.body;
-  if (!username || !email) {
-    throw new ApiError(400, 'Enter a valid username or email');
+  console.log(username, email, password);
+  if (!username || !email || !password) {
+    throw new ApiError(400, 'All fields are required.');
   }
 
   // check if user already exists
   const existingUser = await User.findOne({
     $or: [{ email }, { username }],
   });
+
   if (existingUser) {
     throw new ApiError(400, 'User already exists');
   }
-  // check if password is valid
-  if (!password) {
-    throw new ApiError(400, 'Please enter a valid password');
-  }
 
-  const createdUser = await User.create({
+  const salt = await bcrypt.genSalt(10);
+  const encodedPassword = await bcrypt.hash(password, salt);
+
+  const user = await User.create({
     username,
     email,
-    password,
+    password: encodedPassword,
   });
 
-  if (!createdUser) {
+  if (!user) {
     throw new ApiError(501, 'Internal Server Error');
   }
+
+  const createdUser = await User.findById(user._id).select('-password');
 
   return res.status(201).json(new ApiResponse(201, createdUser, 'User created successfully'));
 });
@@ -43,25 +42,27 @@ const userLogin = requestHandler(async (req, res, next, err) => {
   // required fields to validate
   const { username, email, password } = req.body;
   // validate if fields are empty
-  if (!username && !email) {
-    throw new ApiError(400, 'Please enter a valid email or username');
+  if ((!username && !email) || !password) {
+    throw new ApiError(400, 'All fields are required.');
   }
   // find the user in database (email || username)
   const user = await User.findOne({
     $or: [{ email }, { username }],
   });
 
-  if (!user) {
+  // check the password if valid or not
+  const comparePassword = await bcrypt.compare(password, user.password);
+  if (!comparePassword) {
+    throw new ApiError(401, 'Please enter a valid password.');
+  }
+
+  const loggedInUser = await User.findById(user._id).select('-password');
+
+  if (!loggedInUser) {
     throw new ApiError(404, 'User not found');
   }
-  // check the password if valid or not
-  if (password) {
-    if (password != user.password) {
-      throw new ApiError(400, 'Please enter a valid password');
-    }
-  }
   // return the user | document
-  return res.status(200).json(new ApiResponse(200, user, 'User logged In successfully'));
+  return res.status(200).json(new ApiResponse(200, loggedInUser, 'User logged In successfully'));
 });
 
 export { userRegister, userLogin };
